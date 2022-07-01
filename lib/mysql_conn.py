043c7f37b8191761cb808434d1914e26be56b7a3
Yosutta <phuphuongtin@gmail.com>
2022-07-01 15:01:14 +0700

    Adding freesqldatabase connection

--- lib/mysql_conn.py
+++ lib/mysql_conn.py
@@ -1,8 +1,16 @@
 import mysql.connector
 
+# mysqldb = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="password",
+#     database="chatbot_flask"
+# )
+
 mysqldb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="password",
-    database="chatbot_flask"
+    host="sql6.freesqldatabase.com",
+    user="sql6503307",
+    password="QliwI7kMEg",
+    database="sql6503307",
+    port="3306"
 )
